200ccded3a67843f5979e508dc98df9d6dc52820
rewinery <rewinery@outlook.com>
2024-11-19 15:42:56 +0800

    init lab4 dir

---
+++ lab/lab3/test/test/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test
+//
+//  Created by Qirun Zeng on 2024/10/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
